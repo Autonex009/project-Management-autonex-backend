0563f23dee64001204da84a8982d23df2e1b94fa
DhanashreeS <dhanashree0605@gmail.com>
2026-09-09 19:54:21 +0530

    Fix: Test fail change

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,10 @@
 import os
+
+from sqlalchemy.ext.compiler import compiles
+from sqlalchemy.dialects.postgresql import JSONB
+
+from app.models.daily_checkin import DailyCheckIn
+
 os.environ["JWT_SECRET_KEY"] = "test-secret-key-for-pytest"
 os.environ["OTP_SECRET_KEY"] = "test-otp-secret-key-for-pytest"
 
