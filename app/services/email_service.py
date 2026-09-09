18d4070f41584f0c484882a6e6546557d011987b
DhanashreeS <dhanashree0605@gmail.com>
2026-09-09 19:01:17 +0530

    Fix: Test fail

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -14,11 +14,6 @@ import os
 import urllib.error
 import urllib.request
 import base64
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
-import smtplib  
 
 logger = logging.getLogger(__name__)
 
